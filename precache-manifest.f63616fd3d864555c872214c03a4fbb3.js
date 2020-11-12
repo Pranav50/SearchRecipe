93b0e803626735d4a417fd0824cb16208c90da0e
Pranav Pawar <pranavpwr815@gmail.com>
2020-11-12 18:30:08 +0530

    Updates

---
+++ precache-manifest.f63616fd3d864555c872214c03a4fbb3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f8a5550d501c54621346adfc9f8f400",
+    "url": "/SearchRecipe/index.html"
+  },
+  {
+    "revision": "7c6dd32c2a156b680913",
+    "url": "/SearchRecipe/static/css/2.5262f6cb.chunk.css"
+  },
+  {
+    "revision": "1f11767e3706c82aa9be",
+    "url": "/SearchRecipe/static/css/main.97a94b4d.chunk.css"
+  },
+  {
+    "revision": "7c6dd32c2a156b680913",
+    "url": "/SearchRecipe/static/js/2.8ab6efa4.chunk.js"
+  },
+  {
+    "revision": "e36133330eb02786b65f84d2f139e685",
+    "url": "/SearchRecipe/static/js/2.8ab6efa4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f11767e3706c82aa9be",
+    "url": "/SearchRecipe/static/js/main.f32026c8.chunk.js"
+  },
+  {
+    "revision": "f85e1b52bfbe6552edcb",
+    "url": "/SearchRecipe/static/js/runtime-main.c7f0414b.js"
+  },
+  {
+    "revision": "ad2e0bbfc15ee69ccd5f8b8eba05fd34",
+    "url": "/SearchRecipe/static/media/bg2.ad2e0bbf.jpg"
+  }
+]);
